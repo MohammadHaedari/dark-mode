2477ed5c86a8fa8351744d6bdc8d072caf541563
Mohammad Haedari <74098733+MohammadHaedari@users.noreply.github.com>
2021-04-05 16:10:40 +0300

    Dark Mode

---
+++ script.js
@@ -0,0 +1,39 @@
+function selectMode(mode) {
+    
+
+    if (mode === "dark") {
+        color = "var(--light)";
+        backgroundcolor = "var(--dark)";
+        image = "img/moon.png";
+        label = "Dark Mode";
+        informationscolor = "var(--light)";
+        backgroundinformations = "var(--dark2)";
+        informationstext = "Hover moon if you are bored";
+    } else if (mode === "light") {
+        color = "var(--dark)";
+        backgroundcolor = "var(--light)";
+        image = "img/sun.png";
+        label = "Light Mode";
+        informationscolor = "var(--dark)";
+        backgroundinformations = "var(--light2)";
+        informationstext = "Hover sun if you are bored";
+    } else {
+        color = "var(--light)";
+        backgroundcolor = "linear-gradient(45deg,var(--green),var(--purple),var(--green))";
+        image = "img/worldwide.png"
+        label = "Special Mode";
+        informationscolor = "var(--light)";
+        backgroundinformations = "var(--green)";
+        informationstext = "Hover earth if you are bored";
+    }
+    
+    console.log(label);
+    
+    document.getElementById("theme").style.background = backgroundcolor;
+    document.getElementById("theme").style.color = color;
+    document.getElementById("informations").style.background = backgroundinformations;
+    document.getElementById("informations").style.color = informationscolor;
+    document.getElementById("icon").src = image;
+    document.getElementById("label").innerHTML = label;
+    document.getElementById("informations").innerHTML= informationstext;
+}
